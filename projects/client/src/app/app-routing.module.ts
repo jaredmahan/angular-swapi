12e6cc7d2ca2cd7564527ac40a4a8b69699c7450
Jared Mahan <jaredmahan@gmail.com>
2022-05-24 12:34:17 -0500

    chore: Add a route and redirect (default route) to the application

--- projects/client/src/app/app-routing.module.ts
+++ projects/client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { StarwarsLookupContainer } from "./starwars-lookup/starwars-lookup-container.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "starwars-lookup", component: StarwarsLookupContainer },
+  { path: "", redirectTo: "/starwars-lookup", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
